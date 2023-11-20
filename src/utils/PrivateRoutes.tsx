a3bd28fca010512a1606d94a1610615a52cc1e69
midhun-coddletech <midhun.o@coddletech.com>
2023-11-20 15:32:54 +0530

    update

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import Authorization from './AuthContext';
+import useFetchCartItems from '../custom/fetchCart';
+import useFetchWishlistItems from '../custom/fetchWishlist';
 
 const PrivateRoutes: React.FC = function () {
+  useFetchCartItems();
+  useFetchWishlistItems();
   const token = Authorization();
   const auth = { token: token.isAuthenticated };
   return auth.token ? <Outlet /> : <Navigate to="/login" />;
